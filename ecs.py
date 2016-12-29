e42dda816802cd8724422650fe6e6dc491e2e711
James Turk <james.p.turk@gmail.com>
2016-12-29 11:54:41 -0500

    switch ecs.py to use a config file

--- ecs.py
+++ ecs.py
@@ -6,51 +6,49 @@ ecs = boto3.client('ecs', region_name='us-east-1')
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
 
-# OS specific (not secret, but not useful to anyone outside OS)
-ECS_CLUSTER = 'openstates-scrapers'
-KEY_NAME = 'openstates-master'
-SECURITY_GROUP_IDS = ['sg-74350609']
+def load_config():
+    config = yaml.load(open('config.yaml'))
+    return config
 
-# Amazon's us-east-1 ecs optimized AMI (2016.09)
-ECS_IMAGE_ID = 'ami-6df8fe7a'
-ECS_USER_DATA = '#!/bin/bash\necho ECS_CLUSTER={} >> /etc/ecs/ecs.config'.format(ECS_CLUSTER)
+config = load_config()
 
 
 def make_scraper_task(family,
                       entrypoint,
                       memory_soft=128,
                       name='openstates-scraper',
-                      image='openstates/openstates'
+                      image='openstates/openstates',
+                      environment=None,
                       #cpu=None,
                       #memory=None,
                       ):
     log_stream_prefix = family.lower()
+    main_container = {
+        'name': name,
+        'image': image,
+        'essential': True,
+        'entryPoint': entrypoint,
+        'memoryReservation': memory_soft,
+        'logConfiguration': {
+            "logDriver": "awslogs",
+            "options": {
+                "awslogs-group": "openstates-scrapers",
+                "awslogs-region": "us-east-1",
+                "awslogs-stream-prefix": log_stream_prefix
+            }
+        },
+    }
+
+    # TODO: add CPU/memory limits
+
+    if environment:
+        main_container['environment'] = [{'name': k, 'value': v}
+                                         for k, v in environment.items()]
+
     response = ecs.register_task_definition(
         family=family,
         containerDefinitions=[
-            {
-                'name': name,
-                'image': image,
-                'essential': True,
-                'entryPoint': entrypoint,
-                'memoryReservation': memory_soft,
-                #'cpu': cpu,
-                #'memory': memory,
-                #'environment': [
-                #    {
-                #        'name': 'string',
-                #        'value': 'string'
-                #    },
-                #],
-                'logConfiguration': {
-                    "logDriver": "awslogs",
-                    "options": {
-                        "awslogs-group": "openstates-scrapers",
-                        "awslogs-region": "us-east-1",
-                        "awslogs-stream-prefix": log_stream_prefix
-                    }
-                },
-            }
+            main_container
         ],
     )
     return response
@@ -58,7 +56,7 @@ def make_scraper_task(family,
 
 def run_task(task_definition, started_by):
     response = ecs.run_task(
-        cluster=ECS_CLUSTER,
+        cluster=config['ec2']['ecs_cluster'],
         count=1,
         taskDefinition=task_definition,
         startedBy=started_by,
@@ -83,32 +81,24 @@ def run_task(task_definition, started_by):
 
 
 def create_cluster():
-    ecs.create_cluster(clusterName=ECS_CLUSTER)
+    ecs.create_cluster(clusterName=config['ec2']['ecs_cluster'])
 
 
 def create_instance(instance_type):
+    ecs_user_data = '#!/bin/bash\necho ECS_CLUSTER={} >> /etc/ecs/ecs.config'.format(config['ec2']['ecs_cluster'])
     response = ec2.run_instances(
-        ImageId=ECS_IMAGE_ID,
+        ImageId=config['ec2']['ecs_image_id'],
         MinCount=1,
         MaxCount=1,
-        KeyName=KEY_NAME,
-        SecurityGroupIds=SECURITY_GROUP_IDS,
-        UserData=ECS_USER_DATA,
+        KeyName=config['ec2']['key_name'],
+        SecurityGroupIds=[config['ec2']['security_group_id']],
+        UserData=ecs_user_data,
         InstanceType=instance_type,
         IamInstanceProfile={'Name': 'ecsInstanceRole'},
-        #Placement={
-        #    'AvailabilityZone': 'string',
-        #},
-        #Monitoring={
-        #    'Enabled': True|False
-        #},
         #SubnetId='string',
         #DisableApiTermination=True|False,
         #InstanceInitiatedShutdownBehavior='stop'|'terminate',
-        #PrivateIpAddress='string',
-        #ClientToken='string',
         #AdditionalInfo='string',
-        #EbsOptimized=True|False
     )
     return response
 
@@ -120,8 +110,6 @@ def load_tasks():
             task = yaml.load(f)
             make_scraper_task(task['name'],
                               task['entrypoint'].split(),
-                              memory_soft=task.get('memory_soft', 128)
+                              memory_soft=task.get('memory_soft', 128),
+                              environment=task.get('environment')
                               )
-
-#create_cluster()
-#create_instance('t2.medium')
