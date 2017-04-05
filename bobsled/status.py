93edf73487821f9abef15b93eae559dbff109350
James Turk <james.p.turk@gmail.com>
2017-04-05 00:20:52 -0400

    issue creation restored

--- bobsled/status.py
+++ bobsled/status.py
@@ -1,54 +1,201 @@
 from __future__ import print_function
 import os
+import re
+import shutil
 import datetime
+from collections import defaultdict, OrderedDict
+import boto3
 import github3
 
+from bobsled.dynamo import Run, Status
+from bobsled.templates import render_jinja_template
+
+OUTPUT_DIR = '/tmp/bobsled-output'
 
 
-def state_status(runs):
+def update_status():
+    # update run records in database
+    check_status()
+
+    # update global view
+    write_index_html()
+
+
+def check_status():
+    # check everything that's running
+    runs = {r.task_arn: r for r in Run.status_index.query(Status.Running)}
+
+    if not runs:
+        return
+
+    ecs = boto3.client('ecs', region_name='us-east-1')
+    # we limit this to 100 for AWS, which is fine b/c 100 shouldn't be running at once
+    # if somehow they are, a subsequent run will pick the rest up
+    resp = ecs.describe_tasks(cluster=os.environ['BOBSLED_ECS_CLUSTER'],
+                              tasks=list(runs.keys())[:100])
+
+    # match status to runs
+    for failure in resp['failures']:
+        if failure['reason'] == 'MISSING':
+            update_run_status(runs[failure['arn']])
+        else:
+            raise ValueError('unexpected status {}'.format(failure))
+
+    for task in resp['tasks']:
+        if task['lastStatus'] == 'STOPPED':
+            update_run_status(runs[task['taskArn']])
+        elif task['lastStatus'] in ('RUNNING', 'PENDING'):
+            print('still running', runs[task['taskArn']])
+        else:
+            raise ValueError('unexpected status {}'.format(task))
+
+
+def update_run_status(run):
     CRITICAL = 2
-    WARNING = 1
 
-    today = datetime.date.today()
+    logs = list(get_log_for_run(run))
+    run.end = datetime.datetime.utcnow()
 
-    for state, state_runs in runs.items():
-        bad_in_a_row = 0
-        exception = None
-        args = None
-        scraper = None
+    if contains_error(logs):
+        run.status = Status.Error
+        print(run, '=> error')
+        bad_in_a_row = get_failures(run.job)
 
-        for date, rl in sorted(state_runs.items(), reverse=True):
-            if rl.status == 'empty' and date == today:
-                # don't count a missing run today against scraper
-                continue
-            if rl.status == 'good':
-                break
+        if bad_in_a_row >= CRITICAL:
+            make_issue(run.job, bad_in_a_row, logs)
+    else:
+        run.status = Status.Success
+        print(run, '=> success')
+
+    run.save()
+
+    write_day_html(run.job, run.start.date())
+
+
+def get_failures(job):
+
+    bad_in_a_row = 0
+    # get recent runs in reverse-cron
+    for run in Run.query(job, limit=8, scan_index_forward=False):
+        if run.status == Status.Success:
+            break
+        elif run.status == Status.Error:
             bad_in_a_row += 1
-            if exception is None and rl.runs:
-                args = ' '.join(rl.runs[0]['scraped']['args'])
-                run_records = rl.runs[0]['scraped']['run_record']
-                for rr in run_records:
-                    if 'exception' in rr:
-                        scraper = rr['type']
-                        exception = rr['exception']
 
-        if bad_in_a_row >= WARNING:
-            print('warning for', state, bad_in_a_row)
-            if exception and bad_in_a_row >= CRITICAL:
-                make_issue(state, bad_in_a_row, scraper, args, exception)
+    return bad_in_a_row
 
 
-def make_issue(state, days, scraper_type, args, exception):
+def get_log_for_run(run):
+    logs = boto3.client('logs', region_name='us-east-1')
+
+    pieces = dict(
+        task_name=os.environ['BOBSLED_TASK_NAME'],
+        family=run.job.lower(),
+        task_id=run.task_id,
+    )
+    log_arn = '{family}/{task_name}/{task_id}'.format(**pieces)
+
+    next = None
+
+    while True:
+        extra = {'nextToken': next} if next else {}
+        events = logs.get_log_events(logGroupName=os.environ['BOBSLED_ECS_LOG_GROUP'],
+                                     logStreamName=log_arn, **extra)
+        next = events['nextForwardToken']
+
+        if not events['events']:
+            break
+
+        for event in events['events']:
+            yield event
+
+        if not next:
+            break
+
+
+def contains_error(stream):
+    ERROR_REGEX = re.compile(r'(CRITICAL)|(Exception)|(Traceback)')
+    for line in stream:
+        if ERROR_REGEX.findall(line['message']):
+            return True
+
+
+class RunList(object):
+
+    def __init__(self):
+        self.runs = []
+
+    def add(self, run):
+        self.runs.append(run)
+
+    @property
+    def status(self):
+        has_success = False
+        has_failure = False
+        for r in self.runs:
+            if r.status == Status.Error:
+                has_failure = True
+            elif r.status == Status.Success:
+                has_success = True
+        if has_success and has_failure:
+            return 'other'
+        elif has_success:
+            return 'good'
+        elif has_failure:
+            return 'bad'
+        else:
+            return 'empty'
+
+
+def write_index_html():
+    chart_days = 14
+
+    # get recent runs and group by day
+    runs = Run.recent(chart_days)
+
+    job_runs = defaultdict(lambda: defaultdict(RunList))
+
+    for run in runs:
+        rundate = run.start.date()
+        job_runs[run.job][rundate].add(run)
+
+    # render HTML
+    today = datetime.date.today()
+    days = [today - datetime.timedelta(days=n) for n in range(chart_days)]
+    runs = OrderedDict(sorted(job_runs.items()))
+    html = render_jinja_template('runs.html', runs=runs, days=days)
+
+    try:
+        os.makedirs(OUTPUT_DIR)
+    except OSError:
+        pass
+
+    with open(os.path.join(OUTPUT_DIR, 'index.html'), 'w') as out:
+        out.write(html)
+    shutil.copy(os.path.join(os.path.dirname(__file__), '../css/main.css'), OUTPUT_DIR)
+
+
+def write_day_html(job, date):
+    start = datetime.datetime(date.year, date.month, date.day, 0, 0, 0)
+    end = datetime.datetime(date.year, date.month, date.day, 11, 59, 59)
+    runs = list(Run.query(job, start__between=[start, end]))
+    html = render_jinja_template('day.html', runs=runs, date=date)
+    with open(os.path.join(OUTPUT_DIR,
+                           'run-{}-{}.html'.format(job, date)), 'w') as out:
+                    out.write(html)
+
+
+def make_issue(job, days, logs):
     gh = github3.login(token=os.environ['BOBSLED_GITHUB_KEY'])
     r = gh.repository(os.environ['BOBSLED_GITHUB_USER'],
                       os.environ['BOBSLED_GITHUB_ISSUE_REPO'])
 
     # ensure upper case
-    state = state.upper()
+    job = job.upper()
 
     existing_issues = r.iter_issues(labels='automatic', state='open')
     for issue in existing_issues:
-        if issue.title.startswith(state):
+        if issue.title.startswith(job):
             print('issue already exists: #{}- {}'.format(
                 issue.number, issue.title)
             )
@@ -56,18 +203,20 @@ def make_issue(state, days, scraper_type, args, exception):
 
     since = datetime.date.today() - datetime.timedelta(days=days-1)
 
-    body = '''State: {state} - scraper has been failing since {since}
+    # show last 50 log lines
+    logs = '\n'.join([l['message'] for l in logs[-50:]])
 
-Based on automated runs it appears that {state} has not run successfully in {days} days ({since}).
+    body = '''{job} has been failing since {since}
 
-```{args}``` | **failed during {scraper_type}**
+Based on automated runs it appears that {job} has not run successfully in {days} days ({since}).
+
 
 ```
-  {traceback}
+  {logs}
 ```
 
 Visit http://bobsled.openstates.org/ for more info.
-'''.format(state=state, since=since, days=days, scraper_type=scraper_type, args=args, **exception)
-    title = '{} scraper failing since at least {}'.format(state, since)
+'''.format(job=job, since=since, days=days, logs=logs)
+    title = '{} scraper failing since at least {}'.format(job, since)
     issue = r.create_issue(title=title, body=body, labels=['automatic', 'ready'])
     print('created issue: #{} - {}'.format(issue.number, title))
