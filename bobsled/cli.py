ea532ea8d6eeedb5065928e2272687d316905c7f
James Turk <james.p.turk@gmail.com>
2017-05-05 18:31:46 -0400

    drop old lambda deploy code

--- bobsled/cli.py
+++ bobsled/cli.py
@@ -1,8 +1,6 @@
-import os
 import getpass
 import click
 from .tasks import publish_task_definitions, run_task
-from .awslambda import publish_function
 from .status import update_status, get_log_for_run
 from .dynamo import Run
 
@@ -42,36 +40,9 @@ def run(jobs):
 
 
 @cli.command()
-@click.option('--upload/--no-upload', default=False)
-def status(upload):
+def status():
     update_status()
 
-
-@cli.command()
-def init_lambda():
-    funcs = {
-        'bobsled.handlers.update_status_handler': {
-            'BOBSLED_ECS_CLUSTER': os.environ['BOBSLED_ECS_CLUSTER'],
-            'BOBSLED_TASK_NAME': os.environ['BOBSLED_TASK_NAME'],
-            'BOBSLED_ECS_LOG_GROUP': os.environ['BOBSLED_ECS_LOG_GROUP'],
-            'BOBSLED_GITHUB_KEY': os.environ['BOBSLED_GITHUB_KEY'],
-            'BOBSLED_GITHUB_USER': os.environ['BOBSLED_GITHUB_USER'],
-            'BOBSLED_GITHUB_ISSUE_REPO': os.environ['BOBSLED_GITHUB_ISSUE_REPO'],
-            'BOBSLED_STATUS_BUCKET': os.environ['BOBSLED_STATUS_BUCKET'],
-        },
-        'bobsled.handlers.run_task_handler': {
-            'BOBSLED_ECS_CLUSTER': os.environ['BOBSLED_ECS_CLUSTER'],
-        },
-        'bobsled.handlers.scale_handler': {
-            'BOBSLED_ECS_CLUSTER': os.environ['BOBSLED_ECS_CLUSTER'],
-            'BOBSLED_ECS_KEY_NAME': os.environ['BOBSLED_ECS_KEY_NAME'],
-            'BOBSLED_SECURITY_GROUP_ID': os.environ['BOBSLED_SECURITY_GROUP_ID'],
-            'BOBSLED_CLUSTER_SCHEDULE': os.environ['BOBSLED_CLUSTER_SCHEDULE'],
-        }
-    }
-    for func, env in funcs.items():
-        publish_function(func.replace('.', '-'), func, func,
-                         env, timeout=30, delete_first=True)
 
 if __name__ == '__main__':
     cli()
