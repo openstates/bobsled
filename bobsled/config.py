268e82984339b3a77033460b7d980e39bd64caaf
James Turk <james.p.turk@gmail.com>
2017-05-15 10:50:51 -0400

    able to scale up again

--- bobsled/config.py
+++ bobsled/config.py
@@ -5,16 +5,19 @@ CLUSTER_NAME = os.environ.get('BOBSLED_CLUSTER_NAME', 'bobsled')
 TASK_NAME = os.environ.get('BOBSLED_TASK_NAME', 'bobsled')
 LOG_GROUP = os.environ.get('BOBSLED_ECS_LOG_GROUP', 'bobsled')
 ECS_IMAGE_ID = os.environ.get('BOBSLED_ECS_IMAGE_ID', 'ami-275ffe31')
-ECS_KEY_NAME = os.environ.get('BOBSLED_ECS_KEY_NAME', 'bobsled.pub')
-SECURITY_GROUP_ID = os.environ.get('BOBSLED_SECURITY_GROUP_ID', 'bobsled')
+ECS_KEY_NAME = os.environ.get('BOBSLED_ECS_KEY_NAME', 'bobsled')
 
 # required
+SUBNET_ID = os.environ.get('BOBSLED_SUBNET_ID')
+SECURITY_GROUP_ID = os.environ.get('BOBSLED_SECURITY_GROUP_ID')
+CONFIG_PATH = os.environ.get('BOBSLED_CONFIG_PATH')
 STATUS_BUCKET = os.environ.get('BOBSLED_STATUS_BUCKET')
+
+# required for GitHub features
 GITHUB_USER = os.environ.get('BOBSLED_GITHUB_USER')
 GITHUB_KEY = os.environ.get('BOBSLED_GITHUB_KEY')
 GITHUB_TASK_REPO = os.environ.get('BOBSLED_GITHUB_TASK_REPO')
 GITHUB_ISSUE_REPO = os.environ.get('BOBSLED_GITHUB_ISSUE_REPO')
 
 # ???
-CONFIG_PATH = os.environ.get('BOBSLED_CONFIG_PATH')
 LAMBDA_ARN = os.environ.get('BOBSLED_LAMBDA_ARN')
