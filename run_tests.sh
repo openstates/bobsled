19225f004de60b66ec17ccfcad32d5060e717cc4
James Turk <dev@jamesturk.net>
2020-02-03 23:55:13 -0500

    drop and add db for tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
+PGHOST=localhost PGPORT=5435 PGUSER=bobsled PGPASSWORD=bobsled psql -c 'drop database bobsled_test;'
 PGHOST=localhost PGPORT=5435 PGUSER=bobsled PGPASSWORD=bobsled psql -c 'create database bobsled_test;'
 BOTO_CONFIG=/dev/null AWS_DEFAULT_REGION=us-east-1 BOBSLED_SECRET_KEY="secret-stuff-here" BOBSLED_TASKS_FILENAME="bobsled/tests/tasks/tasks.yml" poetry run pytest $@
