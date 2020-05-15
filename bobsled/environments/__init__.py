0e1a726c6394018bc47064ca8e5cc8ce16a51314
James Turk <dev@jamesturk.net>
2020-05-15 17:18:38 -0400

    add newyaml loader for envs

--- bobsled/environments/__init__.py
+++ bobsled/environments/__init__.py
@@ -1,3 +1,4 @@
 from .yaml_environment import YamlEnvironmentProvider  # noqa
 from .local_environment import LocalEnvironmentProvider  # noqa
 from .paramstore_environment import ParameterStoreEnvironmentProvider  # noqa
+from .newyaml import YamlEnvironmentProvider as NewYamlEnvironmentProvider  # noqa
---
+++ bobsled/environments/newyaml.py
@@ -0,0 +1,51 @@
+import yaml
+import boto3
+from ..base import Environment, EnvironmentProvider
+
+"""
+Format of environment file:
+
+<environment_name>:
+    - variable: <variable_name>
+      hidden: [true|false]
+      paramstore: <paramstore path>
+           OR
+      string: <value>
+"""
+
+
+def paramstore_loader(varname):
+    ssm = boto3.client("ssm")
+    resp = ssm.get_parameters_by_path(Name=varname, WithDecryption=True)
+    return resp["Parameter"]["Value"]
+
+
+class YamlEnvironmentProvider(EnvironmentProvider):
+    def __init__(self, BOBSLED_ENVIRONMENT_FILENAME):
+        self.filename = BOBSLED_ENVIRONMENT_FILENAME
+        self.environments = {}
+
+    def get_environment_names(self):
+        return list(self.environments.keys())
+
+    def get_environment(self, name):
+        return self.environments[name]
+
+    async def update_environments(self):
+        with open(self.filename) as f:
+            data = yaml.safe_load(f)
+
+        for name, envdef in data.items():
+            values = {}
+            for env_var in envdef:
+                if "string" in env_var:
+                    values[env_var["variable"]] = env_var["string"]
+                elif "paramstore" in env_var:
+                    values[env_var["variable"]] = paramstore_loader(
+                        env_var["paramstore"]
+                    )
+                else:
+                    raise ValueError(
+                        f"{name}.{env_var['variable']} must include 'string' or 'paramstore'"
+                    )
+            self.environments[name] = Environment(name, values)
