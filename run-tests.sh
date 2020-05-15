30f601bb7deab05dbaae215c884ca1d9e03431f7
James Turk <dev@jamesturk.net>
2020-05-15 18:26:20 -0400

    add env filename

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
+set ARGS = $@
+
 PGHOST=localhost PGUSER=bobsled PGPASSWORD=bobsled psql -c 'drop database bobsled_test;'
 PGHOST=localhost PGUSER=bobsled PGPASSWORD=bobsled psql -c 'create database bobsled_test;'
-BOTO_CONFIG=/dev/null AWS_DEFAULT_REGION=us-east-1 BOBSLED_SECRET_KEY="secret-stuff-here" BOBSLED_TASKS_FILENAME="bobsled/tests/tasks/tasks.yml" poetry run pytest $@
+export BOTO_CONFIG=/dev/null
+export AWS_DEFAULT_REGION=us-east-1
+export BOBSLED_SECRET_KEY="secret-stuff-here"
+export BOBSLED_TASKS_FILENAME="bobsled/tests/tasks/tasks.yml"
+export BOBSLED_ENVIRONMENT_FILENAME="bobsled/tests/environments.yml"
+poetry run pytest $ARGS
